9cae376ed81e48d5c2575cc963be172dd367705c
Victoria R <vik-rozanova@yandex.ru>
2022-08-18 23:43:23 +0300

    Zadacha Even Number [1,N] full easier without ,

---
+++ Zadacha_All_Even_Easier_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// На вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число, большее 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int evennumber = 2;
+
+// Это упрощённый вариант без запятых
+
+if(N > 1)
+
+{
+    while(evennumber <= N)     // [1,N], т.е. включительно
+    {
+        Console.Write(evennumber + " ");
+        evennumber = evennumber + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Ваше число не подходит условию задачи, невозможно определить чётные числа [1,N]");
+}
