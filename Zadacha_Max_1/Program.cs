b3266d617c8ac4f87c760594ffd2b21d8d4a139f
Victoria R <vik-rozanova@yandex.ru>
2022-08-18 22:54:36 +0300

    Zadacha Max 1

---
+++ Zadacha_Max_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA + " является большим (максимальным) из двух чисел");
+}
+else
+{
+    Console.WriteLine(numberB + " является большим (максимальным) из двух чисел");
+}
