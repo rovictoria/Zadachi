86e666b586e7c0b4edb275d9de1ead965c8dee73
Victoria R <vik-rozanova@yandex.ru>
2022-08-18 23:07:21 +0300

    Zadacha Max 3 1 full

---
+++ Zadacha_Max3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine("max = " + max);
