bc64277a6847b5ca65cdfbfe28c6e20f8e2d2245
Victoria R <vik-rozanova@yandex.ru>
2022-08-18 23:12:41 +0300

    Zadacha Even Number 1 full

---
+++ Zadacha_Even_Number_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if(numberA % 2 == 0)
+{
+    Console.WriteLine(numberA + " - да, это чётное число");
+}
+else
+{
+    Console.WriteLine(numberA + " - нет, это нечётное число");
+}
