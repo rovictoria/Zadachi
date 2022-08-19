ee25378281da8af0baea89895836052432cd4169
Victoria R <vik-rozanova@yandex.ru>
2022-08-19 23:44:26 +0300

    Zadacha Max 1 Longer add full variant with {}

---
+++ Zadacha_Max_Longer_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// найти Max из двух введённых чисел
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+// Фигурные скобки можно было опустить, учитывая что внутри только по одному действию, тренировала
+// логическую насмотренность в большом количестве условий
+
+if(numberA > numberB)
+{
+    Console.WriteLine("max = " + numberA + " (является большим из двух чисел)");
+}
+else
+{
+    if(numberA < numberB)
+    {
+        Console.WriteLine("max = " + numberB + " (является большим из двух чисел)");
+    }
+     else
+     {
+        Console.WriteLine("max = " + numberA + " (числа равны между собой)");
+     }
+}
