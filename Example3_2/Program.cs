3ddd0ac2fb20acaded57429a32f942d3a21f0335
Victoria R <vik-rozanova@yandex.ru>
2022-08-22 21:53:10 +0300

    Example 2 seminar 2 number2 % number1 проверка кратности

---
+++ Example3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает на вход два числа и выводит,
+// является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberB % numberA == 0)
+{
+    Console.WriteLine($"Число {numberB} кратно числу {numberA}");
+}
+else
+{
+    int result = numberB % numberA;
+    Console.WriteLine($"Остаток от деления {numberB} на {numberA} равен {result}");
+}
