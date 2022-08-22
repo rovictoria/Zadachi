a409d53805489b674d7d1a127e47c97a024772d1
Victoria R <vik-rozanova@yandex.ru>
2022-08-23 01:11:51 +0300

    Zadacha 2_2 third number in every number

---
+++ Zadacha2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+
+Console.Write("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(number);
+
+if(number > 99)
+{
+    Console.WriteLine($"Третья цифра числа {number} - {str[2]}");
+}
+else
+{
+    Console.WriteLine($"В {number} нет третьей цифры");  //можно вывести {string}, но больше для схожести кодов вывода написала {number}
+}
