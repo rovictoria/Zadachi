20908d93ef433693ff5e7d1b3f5b99086395dbf7
Victoria R <vik-rozanova@yandex.ru>
2022-08-22 23:59:30 +0300

    Example 2_2 delete number2

--- Example2_2/Program.cs
+++ Example2_2/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 int number = new Random().Next(100,1000);
 int number3 = number % 10;
-int number2 = number % 100;
 int number1 = number / 100;
 
 Console.WriteLine("Было " + number );
