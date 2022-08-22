a486df6dc145e1417dcab05012b84b046e89bc04
Victoria R <vik-rozanova@yandex.ru>
2022-08-22 21:28:25 +0300

    Example 2 seminar 2 from *** to ** number

---
+++ Example2_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int number = new Random().Next(100,1000);
+int number3 = number % 10;
+int number2 = number % 100;
+int number1 = number / 100;
+Console.WriteLine("Было " + number );
+Console.WriteLine("Cтало " + number1 + number3 );
