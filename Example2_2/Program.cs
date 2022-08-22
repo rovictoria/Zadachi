c4bdbd57f6916b6f4b9d8a89bb083c1b25fb9b5c
Victoria R <vik-rozanova@yandex.ru>
2022-08-22 21:37:51 +0300

    Example 2 seminar 2 from *** to ** number add second variant

--- Example2_2/Program.cs
+++ Example2_2/Program.cs
@@ -3,5 +3,12 @@ int number = new Random().Next(100,1000);
 int number3 = number % 10;
 int number2 = number % 100;
 int number1 = number / 100;
+
 Console.WriteLine("Было " + number );
 Console.WriteLine("Cтало " + number1 + number3 );
+
+// Или другой вариант: сумма двухзначного + однозначного 
+// int number11 = new Random().Next(100,1000);
+// int result = ((number11 / 100) * 10) + (number11 % 10);
+
+// Console.WriteLine($"Number = {number11}; result = {result} ");
