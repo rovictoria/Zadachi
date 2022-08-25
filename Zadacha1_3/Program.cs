7a48070cff5ee316300e8cf91d6a8913843689c7
Victoria R <vik-rozanova@yandex.ru>
2022-08-25 23:59:18 +0300

    Zadacha1_3 check

--- Zadacha1_3/Program.cs
+++ Zadacha1_3/Program.cs
@@ -3,19 +3,20 @@
 // 12821 - да
 void Polindrom()
 {
-  Console.Write("Введите пятизначное число: ");
-  int number = Convert.ToInt32(Console.ReadLine());
-  
-  if (number > 9999 && number < 100000) {
-  string num = Convert.ToString(number);
+
+   Console.Write("Введите пятизначное число: ");
+   int number = Convert.ToInt32(Console.ReadLine());
+
+   if (number > 9999 && number < 100000) {
+     string num = Convert.ToString(number);
      if (num[0] == num[4] && num[1] == num[3]) {
-    Console.WriteLine($"{num} число-полиндром");
-    } else {
-      Console.WriteLine($"{num} НЕ число-полиндром");
-    }
-  } else {
-    Console.WriteLine("Это не пятизначное число, попробуйте снова");
-  }
+       Console.WriteLine($"{num} число-полиндром");
+     } else {
+       Console.WriteLine($"{num} НЕ число-полиндром");
+     }
+   } else {
+       Console.WriteLine("Это не пятизначное число, попробуйте снова");
+   } 
 }
 
 Polindrom();
