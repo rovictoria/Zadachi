3a07f24af03476df0be56f5853011dd087e9a748
Victoria R <vik-rozanova@yandex.ru>
2022-08-25 22:29:21 +0300

    Example 1_3 x,y -> quater

---
+++ Example1_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+    
+Console.Write("Введите Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (X < 0 && Y > 0)
+{
+   Console.WriteLine("2 четверть");  
+}
+else if (X < 0 && Y < 0)
+{
+   Console.WriteLine("3 четверть");
+}
+else if (X > 0 && Y < 0)
+{
+   Console.WriteLine("4 четверть");
+}
