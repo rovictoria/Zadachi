4ac9d56ff65c3564b671e7e78bd9347f8db677cb
Victoria R <vik-rozanova@yandex.ru>
2022-08-25 23:49:29 +0300

    Zadacha3_3 void

--- Zadacha3_3/Program.cs
+++ Zadacha3_3/Program.cs
@@ -1,19 +1,24 @@
 ﻿// Программa, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 
-Console.Write("Введите число >= 1: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+void Cubes()
+{
+   Console.Write("Введите число >= 1: ");
+   int N = Convert.ToInt32(Console.ReadLine());
+   int i = 1;
 
-if (N > 0) {
-  while (i < N)
-  {
-    Console.Write(Math.Pow(i,3)+ ", ");
-    i++;
-  }
-  if(i == N) {
-    Console.Write(Math.Pow(i,3));
-  }
-} else {
-    Console.WriteLine("Число не удовлетворяет условию, повторите ввод ");
+   if (N > 0) {
+     while (i < N)
+     {
+       Console.Write(Math.Pow(i,3)+ ", ");
+       i++;
+     }
+     if(i == N) {
+       Console.Write(Math.Pow(i,3));
+     }
+   } else {
+       Console.WriteLine("Число не удовлетворяет условию, повторите ввод ");
+   }
 }
+
+Cubes();
