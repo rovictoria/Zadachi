30a3d17d24ca08092f0cadcd10dc6f23a00ab9cc
Victoria R <vik-rozanova@yandex.ru>
2022-08-25 23:47:25 +0300

    Zadacha2_3 void

--- Zadacha2_3/Program.cs
+++ Zadacha2_3/Program.cs
@@ -2,7 +2,9 @@
 // и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 
-Console.Write("Введите X1 точки А: ");
+void AB()
+{
+    Console.Write("Введите X1 точки А: ");
 int X1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите Y1 точки А: ");
@@ -23,3 +25,6 @@ int Z2 = Convert.ToInt32(Console.ReadLine());
 
 double lengthAB = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
 Console.WriteLine($"Длина отрезка AB между точками A({X1},{Y1},{Z1}) и B({X2},{Y2},{Z2}) равна {lengthAB:f2}");   //  :f2  - сколько знаков после запятой 
+}
+
+AB();
