d7d90b796b950af718a39a83a8cd7392e456ecc9
Victoria R <vik-rozanova@yandex.ru>
2022-08-26 00:11:15 +0300

    example for 5.0

---
+++ Example1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Пример для вставки для форматирования под 5.0
+
+using System;
+
+namespace Example1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
