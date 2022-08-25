26f5c9040b12b7e5b2cd4cdfcd1df27eb78c62e7
Victoria R <vik-rozanova@yandex.ru>
2022-08-25 22:56:53 +0300

    Zadacha1_3  polindrom

---
+++ Zadacha1_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// На вход пятизначное. Проверка на полиндром.
+// 12821 - да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000) {
+  string num = Convert.ToString(number);
+  if (num[0] == num[4] && num[1] == num[3]) {
+    Console.WriteLine($"{num} число-полиндром");
+  } else {
+    Console.WriteLine($"{num} НЕ число-полиндром");
+  }
+} else {
+    Console.WriteLine("Это не пятизначное число, попробуйте снова");
+}
