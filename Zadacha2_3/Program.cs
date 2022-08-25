eeef3ee531b656058bec0529fb3a103b1b4a6042
Victoria R <vik-rozanova@yandex.ru>
2022-08-25 23:50:24 +0300

    Zadacha2_3 check

--- Zadacha2_3/Program.cs
+++ Zadacha2_3/Program.cs
@@ -5,26 +5,25 @@
 void AB()
 {
     Console.Write("Введите X1 точки А: ");
-int X1 = Convert.ToInt32(Console.ReadLine());
+    int X1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите Y1 точки А: ");
-int Y1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Y1 точки А: ");
+    int Y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите Z1 точки А: ");
-int Z1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Z1 точки А: ");
+    int Z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите X2 точки В: ");
-int X2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите X2 точки В: ");
+    int X2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите Y2 точки В: ");
-int Y2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Y2 точки В: ");
+    int Y2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите Z2 точки В: ");
-int Z2 = Convert.ToInt32(Console.ReadLine());
-
-
-double lengthAB = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
-Console.WriteLine($"Длина отрезка AB между точками A({X1},{Y1},{Z1}) и B({X2},{Y2},{Z2}) равна {lengthAB:f2}");   //  :f2  - сколько знаков после запятой 
+    Console.Write("Введите Z2 точки В: ");
+    int Z2 = Convert.ToInt32(Console.ReadLine());
+    
+    double lengthAB = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
+    Console.WriteLine($"Длина отрезка AB между точками A({X1},{Y1},{Z1}) и B({X2},{Y2},{Z2}) равна {lengthAB:f2}");   //  :f2  - сколько знаков после запятой 
 }
 
 AB();
