02a316390e1006fdf7be7ebfd48501b1de4556cc
Victoria R <vik-rozanova@yandex.ru>
2022-08-29 21:40:43 +0300

    Zadacha3_4 fill and print array (for/while) + 2 variants Printarray

---
+++ Zadacha3_4/Program.cs
@@ -0,0 +1,46 @@
+﻿// Программа, которая задаёт массив
+// из 8 элементов и выводит их на экран
+
+void FillArray(int[] numbers)
+{
+    int length = numbers.Length;
+
+    for (int index = 0; index < length; index++)
+    {
+        numbers[index] = new Random().Next(-10000, 10000);
+    }
+}
+
+// void PrintArray(int[] num)
+// {
+//     int length1 = num.Length;
+
+//     for (int position = 0; position < (length1 - 1); position++)
+//     {
+//         Console.Write($"{num[position]}   ");
+//     }
+    
+// }
+
+
+// А если нужно через запятую и с {}
+
+void PrintArray(int[] num)
+{
+    int length1 = num.Length;
+    int position = 0;
+    Console.Write("{ ");
+
+    while (position < (length1 - 1))
+    {
+        Console.Write($"{num[position]}, ");
+        position++;
+    }
+    Console.Write(num[position] + " }");
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+
+PrintArray(array);
