7abe167db2b54f5a65f1d5ffe4cb420baa0e9020
Victoria R <vik-rozanova@yandex.ru>
2022-08-29 23:36:01 +0300

    Zadacha2_4 sum of each number in every number 123 - 6 for all int numbers

---
+++ Zadacha2_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+
+void SumNumbers()
+
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    if (number < 0) 
+    {
+        number = number * (-1);
+    }
+    
+    while (number > 0)
+    {
+        int num = number % 10;
+        number = number / 10;
+        sum = sum + num;
+    }
+
+    Console.WriteLine(sum);
+}
+
+SumNumbers();
