ebe0995b90e15b161a59c18e8d4118f1189705f7
Victoria R <vik-rozanova@yandex.ru>
2022-08-29 22:56:12 +0300

    Example1_4 13442-5 count numbers

---
+++ Example1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая принимает на вход число и выдаёт количество цифр в числе.
+void SumCount()
+{
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = (int) Math.Log10(number) + 1;
+Console.WriteLine(count);
+}
+SumCount();
